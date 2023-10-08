0fe93462f5a89200cf099d00225987b8e492e2f0
Isaiah-Ambi <ambiisaiah24@gmail.com>
2023-10-08 23:12:47 +0100

    initial commit

---
+++ core/templates/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./core/**/*.{html,js}"],
+  purge: [
+    './core/**/*.{html,js}',
+    './core/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
